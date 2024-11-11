a74168f60c5790d5250e25c37cf5f59bd46273ff
NickalasLight <nickalas.93@gmail.com>
2024-11-11 17:27:53 +0100

    initial boilerplate for making a world, world blocks, buildings, npcs, etc. For now will run a .cs file for a world, eventually will set up to accept json settings for everything

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//For now, we will create simple world that is 10 by 10 grid
+/*
+ 
+
+0,0
+
+
+
+
+ 
+I
+I
+I
+I_______
+ 
+ 
+ 
+ */
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
