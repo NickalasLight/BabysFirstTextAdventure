cba71783ca3243bcddd674420d037a33f73b7ae9
NickalasLight <nickalas.93@gmail.com>
2024-11-12 18:16:44 +0400

    WIll need to test later tonight, but in theory basic world and basic user has been built and should take move commands and the basic describe command

--- Program.cs
+++ Program.cs
@@ -16,16 +16,107 @@ I_______
  
  
  */
+//For now we will gen both the world and the user here in main.
+//The most obvious improvement is the use of config files, in a readable format for different save files
+
+//Create new world with 4 world blocks
+World myWorld = new World();
+
+myWorld.AddBlock
+(
+
+
+new WorldBlock(
+    coordinate: new Coordinate(x:0,y:0),
+    descriptions:new List<string>{"this is the world at 0,0"},
+    gameObjects:new List<GameObject>{},
+    gamePois:new List<GamePoi>{},
+    gameBuildings:new List<GameBuilding>{},
+    gameNpcs:new List<GameNpc>{}
+    )
+
+);
+
+myWorld.AddBlock
+(
+
+
+new WorldBlock(
+    coordinate: new Coordinate(x:0,y:1),
+    descriptions:new List<string>{"this is the world at 0,1"},
+    gameObjects:new List<GameObject>{},
+    gamePois:new List<GamePoi>{},
+    gameBuildings:new List<GameBuilding>{},
+    gameNpcs:new List<GameNpc>{}
+    )
+
+);
+
+myWorld.AddBlock
+(
+
+
+new WorldBlock(
+    coordinate: new Coordinate(x:0,y:1),
+    descriptions:new List<string>{"this is the world at 1,0"},
+    gameObjects:new List<GameObject>{},
+    gamePois:new List<GamePoi>{},
+    gameBuildings:new List<GameBuilding>{},
+    gameNpcs:new List<GameNpc>{}
+    )
+
+);
+
+myWorld.AddBlock
+(
+
+
+new WorldBlock(
+    coordinate: new Coordinate(x:0,y:1),
+    descriptions:new List<string>{"this is the world at 1,1"},
+    gameObjects:new List<GameObject>{},
+    gamePois:new List<GamePoi>{},
+    gameBuildings:new List<GameBuilding>{},
+    gameNpcs:new List<GameNpc>{}
+    )
+
+);
+
+User myUser = new User(
+name:"nick",
+description:"the game developer",
+homeCountry:"USA",
+language:"English",
+promptMemory:"",
+inventory: new List<object>{},
+location: new Coordinate(0,0)
+
+
+
+
+);
+
+
+
+
+
+//Create a new user with the location at the southwest (0,0)
+
+
+
+
+
+
 
 
 
  //Takes input from user
  //lets add a simple set off verbs as a switch statement class, I guess? 
 string myVerb;
-ActionInterpreter interpreter = new ActionInterpreter();
+ActionInterpreter interpreter = new ActionInterpreter(myWorld,myUser);
 While(!="ExitGame")
 {
-
+//The classic enternal while loop for a game
 myVerb = Console.ReadLine();
 interpreter(myVerb);
 
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 
+llm 1 --> batch of text commands instead of like classic text games, the goal being to fine tune a model to convert the text to the proper command set. 
+Best infrastructure for using an llm that is meant to more easily translate human text to be useable by the switch statement in the interpreter class. I go into the neartest toolstore, I ask the shopkeeper "Wie Viel Kostet" and I buy a wrench with my money --> instead of, Enter ToolShop, Grab Wrench, Talk to ShopOwner, Speak "Wie Viel Kostet?", Pay 10 euro,
 
+llm2 --> The "TimeMachine" is providing translations for phrases
+llm3 --> is the ai for the npc's
 
+all of these can be the same or different llms, but are the 3 specific use cases for llms that I can currently see as being potentially useful. 
+
+----> seems like a pretty reasonable usecase for an llm, presumably training it to create a series of commands from an input that can be executed in order. 
 
 
 --------
@@ -16,11 +23,12 @@ So....
 6. I think a way to track progress, will be a big piece
 7. I think some sort of language grading/testing to give some unoffficial language level. Can also be useful
 8. Will want the interaction with the terminal/time machine to be interesting. 
+9. Everything needs a lot of cleanup, of course. 
 
 
 -----
 
-Create user class
+Create user class --> check
 Create basic class that just builds simple world --> transition to json config or other config language based world construction. 
 create terminal in main poi. 
 Integration of an llm is probably easiest with chatgpt4 per now, and we can dedicate maybe 50 euro for this purpose for now. 
